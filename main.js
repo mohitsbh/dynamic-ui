11a2d428924915ec489d15ab4512b3959526785e
mohitsbh <143165103+mohitsbh@users.noreply.github.com>
2024-12-11 16:45:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>190){
+            $('header').addClass('scrolled');
+        }
+        else{
+            $('header').removeClass('scrolled'); 
+        }
+    });
+})
